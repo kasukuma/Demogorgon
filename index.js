228276fa63f06a7f4d3d4501b9f564a787ae3365
kasukuma <fnsvkt2qkv@privaterelay.appleid.com>
2025-10-24 08:59:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ import path from 'node:path';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import ejs from 'ejs';
 import axios from 'axios';
 import miniget from 'miniget';
-import ytpl from 'ytpl';
-import ytsr from 'ytsr';
 import bodyParser from 'body-parser';
 
 const __dirname = process.cwd();
